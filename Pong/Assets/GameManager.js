810b6d4377a30d7b5925ace0a10495127c0546d6
Durranos <david.kearney@cfl.rr.com>
2014-07-19 23:48:51 -0400

    Updated Game Code and Assets
    Updated Game Code and Assets

---
+++ Pong/Assets/GameManager.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+static var playerScore01 : int = 0;
+static var playerScore02 : int = 0;
+
+var scoreSkin : GUISkin;
+
+static function Score (wallName : String) {
+	if(wallName == "rightWall"){
+		playerScore01 += 1;	
+	}
+	else if (wallName == "leftWall"){
+		playerScore02 += 1;
+	}
+}
+
+function OnGUI(){
+	GUI.skin = scoreSkin;
+	GUI.Label (new Rect (Screen.width/2-150-12, 25, 100, 100), "" + playerScore01);
+	GUI.Label (new Rect (Screen.width/2+150-12, 25, 100, 100), "" + playerScore02);
+}
